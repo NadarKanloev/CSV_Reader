048be066283295ad4ec1d649d30157b254f343ee
Надар Канлоев <donat290817@gmail.com>
2022-01-02 16:22:07 +0300

    Добавил Класс

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,44 +7,11 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
+import static com.company.Work_With_CSV.input;
+import static com.company.Work_With_CSV.readcsv;
+
 public class Main
 {
-    public static  String input()
-    {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Вы будете работать ");
-        System.out.println("Введите путь файла");
-        String path = in.nextLine();
-        return path;
-    }
-    public static void readcsv(String path)
-    {
-        String line = "";
-        try
-        {
-            int counter = 0;
-            int width = 0;
-            BufferedReader br = new BufferedReader(new FileReader(path));
-            while((line = br.readLine())!= null)
-            {
-                counter=+1;
-                width = (line.split(",")).length;
-
-            }
-
-
-        }
-        catch(FileNotFoundException e)
-        {
-            e.printStackTrace();
-            System.out.println("Файл Не Найден");
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-
-    }
 
     public static void main(String[] args)
     {
