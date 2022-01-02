54731ea5e9fee08491117bddf026106b21300f6c
Надар Канлоев <donat290817@gmail.com>
2022-01-02 14:16:25 +0300

    CHECK

--- src/com/company/ReadCSV.java
+++ src/com/company/ReadCSV.java
@@ -6,5 +6,5 @@ import java.io.FileReader;
 
 public class ReadCSV
 {
-  CSVReader reader = new CSVReader(new FileReader("transactions_cut.csv"), ',' , '"' , 1);
+  CSVReader reader = new CSVReader(new ощшощгштфщтсщхоТВФ FileReader("transactions_cut.csv"), ',' , '"' , 1);
 }
